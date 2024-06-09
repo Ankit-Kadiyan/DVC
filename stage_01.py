b19f6c8b87217ba428cd91d80913688f85111ac8
tavi1402 <tavi1402@gmail.com>
2024-06-09 13:42:39 +0530

    Final Commit

---
+++ stage_01.py
@@ -0,0 +1,5 @@
+with open("artifacts.txt","w") as f:
+    f.write("My name is Ankit and here i am executing stage one")
+
+with open("artifacts2.txt","w") as f:
+    f.write("This is my second file")
---
+++ stage_02.py
@@ -0,0 +1,7 @@
+with open("artifacts.txt","r") as f:
+    text=f.read()
+print(text)
+
+print('I am Studing DVC.')
+
+print("This is the second example.")
